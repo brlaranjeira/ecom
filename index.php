dcda9de02fdc8939c1349b1acd351c27e8fbd60c
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-09-25 19:57:46 -0300

    inicia home

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brlaranjeira
+ * Date: 25/09/18
+ * Time: 19:45
+ */
+
+?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        .sec-header{
+            height: 100vh;
+            min-height: 650px;
+            padding-top: 0;
+            padding-bottom: 0;
+            background-color: #aa3d4d;
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body style="margin: 0">
+<div class="container-fluid">
+    <header class="sec-header">
+        aa
+    </header>
+    <section>
+        asdasdasda
+    </section>
+</div>
+</body>
+</html>
