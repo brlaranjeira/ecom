95aed1f28ac5e981e29dc0770d47f5ff0647535b
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-09-26 09:21:26 -0300

    move estilos para arquivo separado

--- index.php
+++ index.php
@@ -12,25 +12,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        .sec-header{
-            height: 100vh;
-            min-height: 650px;
-            padding-top: 0;
-            padding-bottom: 0;
-            background-color: #aa3d4d;
-            background-image: url("./img/spoiler.png");
-            background-position: center;
-            background-repeat: no-repeat;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/index.css">
     <title>Document</title>
 </head>
 <body style="margin: 0">
 <div class="container-fluid">
-    <header class="sec-header">
+    <section class="sec-header">
 
-    </header>
+    </section>
     <section>
         asdasdasda
     </section>
